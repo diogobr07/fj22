dd1476bd0ed9f1fe2a24dec731f2adb0977a38e5
diogo <diogobruno07@gmail.com>
2013-12-05 22:37:46 -0200

    first jsf components

--- src/main/java/br/com/caelum/argentum/ws/ClienteWebService.java
+++ src/main/java/br/com/caelum/argentum/ws/ClienteWebService.java
@@ -11,22 +11,22 @@ import br.com.caelum.argentum.reader.LeitorXML;
 
 public class ClienteWebService {
 	private static final String URL_WEBSERVICE = "http://argentumws.caelum.com.br/neogociacoes";
-	
-	public List<Negociacao> getNegociacoes(){
+
+	public List<Negociacao> getNegociacoes() {
 		HttpURLConnection connection = null;
 		try {
-		URL url = new URL(URL_WEBSERVICE);
-		
-		connection = (HttpURLConnection) url.openConnection();
-		
-		InputStream content = connection.getInputStream();
-		
-		return new LeitorXML().carrega(content);
+			URL url = new URL(URL_WEBSERVICE);
+
+			connection = (HttpURLConnection) url.openConnection();
+
+			InputStream content = connection.getInputStream();
+
+			return new LeitorXML().carrega(content);
 		} catch (IOException e) {
 			throw new RuntimeException(e);
 		} finally {
 			connection.disconnect();
 		}
-		
+
 	}
 }
